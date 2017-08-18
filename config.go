cdf6cc58302cd1a0ee23d43ca942d1d71b308010
paul wisehart <paul@oldcode.org>
2017-08-18 17:42:50 -0400

    reworked to not put everything into init()

--- config.go
+++ config.go
@@ -7,21 +7,26 @@ import (
 )
 
 var GOPATH string
-var configPath string
+var baseDir string
 
 func init() {
-	fmt.Println("init config")
-
 	viper.BindEnv("GOPATH")
 	GOPATH = viper.GetString("GOPATH")
+}
 
-	viper.SetDefault("template_dir", fmt.Sprintf("%s/src/oldcode.org/webplay/template", GOPATH))
-	viper.SetDefault("static_dir", fmt.Sprintf("%s/src/oldcode.org/webplay/static", GOPATH))
-	viper.SetDefault("work_dir", fmt.Sprintf("%s/src/oldcode.org/webplay/work", GOPATH))
-	viper.SetDefault("log_file", fmt.Sprintf("%s/src/oldcode.org/webplay/log.txt", GOPATH))
-	viper.SetDefault("db_file", fmt.Sprintf("%s/src/oldcode.org/webplay/database.db", GOPATH))
+func Init(path string) {
+	baseDir = fmt.Sprintf("%s/src/%s", GOPATH, path)
+	fmt.Printf("baseDir:%s\n", baseDir)
+	Read()
+}
 
-	//read_settings()
+func InitWeb(path string) {
+	Init(path)
+	viper.SetDefault("template_dir", fmt.Sprintf("%s/template", baseDir))
+	viper.SetDefault("static_dir", fmt.Sprintf("%s/static", baseDir))
+	viper.SetDefault("work_dir", fmt.Sprintf("%s/work", baseDir))
+	viper.SetDefault("log_file", fmt.Sprintf("%s/log.txt", baseDir))
+	viper.SetDefault("db_file", fmt.Sprintf("%s/database.db", baseDir))
 }
 
 func Get(name string) string {
@@ -36,32 +41,11 @@ func GetInt(name string) int {
 	return viper.GetInt(name)
 }
 
-func SetDir(path string) {
-	configPath = path
-}
-
 func Read() {
-	//_, err := os.Getwd()
-	//if err != nil {
-	//	fmt.Println(err)
-	//	os.Exit(1)
-	//}
-	//_, filename, _, ok := runtime.Caller(1)
-	//if !ok {
-	//	panic("error finding settings file")
-	//}
-	//fmt.Println(filename)
-	//filepath := path.Dir(path.Join(filename, ".."))
-	//fmt.Println(filepath)
-	viper.AddConfigPath(configPath)
-
+	viper.AddConfigPath(baseDir)
 	viper.SetConfigName("settings")
-
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic("error finding settings file 2")
 	}
-	template_dir := viper.GetString("template_dir")
-	fmt.Printf("template_dir: %s\n", template_dir)
-
 }
