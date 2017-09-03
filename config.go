c6c697962bc2170e6978d44c6d8ec878bef72117
paul wisehart <paul@oldcode.org>
2017-09-02 23:09:51 -0400

    hack, need to be more robust about where settings is

--- config.go
+++ config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"os"
+	"path"
 
 	"github.com/spf13/viper"
 )
@@ -13,22 +14,30 @@ var baseDir string
 func init() {
 	viper.BindEnv("GOPATH")
 	GOPATH = viper.GetString("GOPATH")
-	baseDir, err := os.Getwd()
+	var err error
+	baseDir, err = os.Getwd()
 	if err != nil {
 		panic(fmt.Errorf("config.init() err:%s", err))
 	}
-	fmt.Printf("baseDir:%s\n", baseDir)
+	//fmt.Printf("baseDir:%s\n", baseDir)
 
 	viper.AddConfigPath(baseDir)
+	// this came up in testing.  this package can load from anywhere (pwd speaking)
+	viper.AddConfigPath(path.Clean(fmt.Sprintf("%s/../", baseDir)))
 	viper.SetConfigName("settings")
-	Read()
+	read()
 
+	//fmt.Printf("baseDir2:%s\n", baseDir)
 	viper.SetDefault("template_dir", fmt.Sprintf("%s/template", baseDir))
 	viper.SetDefault("static_dir", fmt.Sprintf("%s/static", baseDir))
 	viper.SetDefault("work_dir", fmt.Sprintf("%s/work", baseDir))
 	viper.SetDefault("log_file", fmt.Sprintf("%s/log.txt", baseDir))
 	viper.SetDefault("db_file", fmt.Sprintf("%s/database.db", baseDir))
 	viper.SetDefault("pongo2_debug", true)
+}
+
+func settingsDir() string {
+	return "asd"
 }
 
 func Get(name string) string {
@@ -43,9 +52,13 @@ func GetInt(name string) int {
 	return viper.GetInt(name)
 }
 
-func Read() {
+func read() {
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("config.Read() err:%s", err))
 	}
+	//fmt.Printf("config:%s\n", viper.ConfigFileUsed())
+	//fmt.Printf("config:%s\n", path.Dir(viper.ConfigFileUsed()))
+	baseDir = path.Dir(viper.ConfigFileUsed())
+	//fmt.Printf("baseDir3:%s\n", baseDir)
 }
